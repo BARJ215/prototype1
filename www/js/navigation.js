a06c198a65da18c2c0c394ffd779049d99350e25
BARJ215 <32267193+BARJ215@users.noreply.github.com>
2018-04-05 20:39:17 +0100

    fix

--- www/js/navigation.js
+++ www/js/navigation.js
@@ -2,11 +2,11 @@ $(document).on("swipeleft","#main", function(){
     $.mobile.pageContainer.pagecontainer( "change","#courseList", { transition: "slideright"}); 
 });
 $(document).on("swiperight","#courseList", function(){
-    $.mobile.pageContainer.pagecontainer( "change" "#main", { transition: "slideleft"}); 
+    $.mobile.pageContainer.pagecontainer( "change", "#main", { transition: "slideleft"}); 
 });
 $(document).on("swipeleft","#courseList", function(){
-    $.mobile.pageContainer.pagecontainer( "change" "#leaderboard", { transition: "slideright" }); 
+    $.mobile.pageContainer.pagecontainer( "change", "#leaderboard", { transition: "slideright" }); 
 });
 $(document).on("swiperight","#leaderboard", function(){
-    $.mobile.pageContainer.pagecontainer( "change" "#courseList", { transition: "slideleft"}); 
+    $.mobile.pageContainer.pagecontainer( "change" ,"#courseList", { transition: "slideleft"}); 
 });
