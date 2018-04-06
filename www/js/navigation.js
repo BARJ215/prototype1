5d93ca4453eb9660cfd22e299a6e4c03a40abc76
BARJ215 <32267193+BARJ215@users.noreply.github.com>
2018-04-06 13:59:18 +0100

    test

--- www/js/navigation.js
+++ www/js/navigation.js
@@ -1,6 +1,11 @@
 $(document).on("swipeleft","#main", function(){
     console.log("swipe");
-    $.mobile.changePage("#courseList", { transition: "slideright"}); 
+    //$.mobile.changePage("#courseList", { transition: "slideright"});
+   
+    $.mobile.pagecontainer( "change", "#courseList", {
+            transition: "slide",
+            reverse: true
+    });
 });
 $(document).on("swiperight","#courseList", function(){
     $.mobile.changePage("#main", { transition: "slideleft"});
